eb96c054d6a8288006e57b2bea4b7f8cda8fe900
Carl Baumann <carlbaumann@Carls-MacBook-Air.local>
2018-11-07 20:22:02 -0600

    Removed excess comment

--- MovingPlatform.cs
+++ MovingPlatform.cs
@@ -10,15 +10,6 @@ public class MovingPlatform : Node2D
 	
 	public override void _PhysicsProcess(float delta)
 	{
-		/*
-	accum += delta * (1.0 / cycle) * PI * 2.0
-	accum = fmod(accum, PI * 2.0)
-	var d = sin(accum)
-	var xf = Transform2D()
-	xf[2]= motion * d
-	$platform.transform = xf
-
-		*/
 		_accum += delta * (1.0f / Cycle) * Mathf.Pi * 2.0f;
 		_accum = _accum % (Mathf.Pi * 2.0f);
 		float d = Mathf.Sin(_accum);
